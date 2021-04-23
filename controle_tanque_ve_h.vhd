625dcf1db1c52893d0f09da0d01be747d5dbf95a
unknown <snunes.aline@gmail.com>
2021-04-23 08:41:08 -0300

    feat: atualizando vhdl da valvula de entrada e elemento de aquecimento

--- controle_tanque_ve_h.vhd
+++ controle_tanque_ve_h.vhd
@@ -28,9 +28,9 @@ entity controle_tanque is
 		Th			   : IN std_logic;
       Tc			   : IN std_logic;
 		Ve		      : OUT std_logic;
-		Vs	         : OUT std_logic;
-		H	         : OUT std_logic;
-		A		      : OUT std_logic
+		--Vs	         : OUT std_logic;
+		H	         : OUT std_logic--;
+		--A		      : OUT std_logic
 		);
 end controle_tanque;
 
@@ -79,9 +79,9 @@ begin
 			 aux1_s <= NOT Lh; -- /Lh
 			 aux2_s <= NOT Tc; -- /Tc
 			 aux3_s <= NOT Th; -- /Th
-			 aux4_s <= aux2_s OR aux3_s; -- /Tc + /Th
+			 aux4_s <= aux2_s OR aux3_s; -- /Tc + /Th                  ta dando U
 			 Ve <= aux1_s AND aux4_s; -- /Lh (/Tc + /Th)
-			 aux5_s <= LI OR aux1_s; --LI + /Lh
+			 aux5_s <= LI OR aux1_s; --LI + /Lh                 ta dando U
 			 aux6_s <= aux3_s AND Tc; --/Th.Tc
 			 H <= aux6_s AND aux5_s;
 	  end process;
