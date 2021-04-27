96cb7fc0f8a29fef9a4e1a201aeb0587ad37ff86
Davi Schmitz <davi.schmitz@hotmail.com>
2021-04-27 12:05:15 -0300

    Add rtl pdf and vhdl code

--- Atividade_2_Semaforo/semaforo/semaforo.vhd
+++ Atividade_2_Semaforo/semaforo/semaforo.vhd
@@ -2,7 +2,7 @@
 -- UNISINOS - Universidade do Vale do Rio dos Sinos 
 -- DESIGN NAME  : semaforo.vhd
 -- NAME         : 
--- PROJECT      : Controle de um semáforo de trânsito
+-- PROJECT      : Controle de um semï¿½foro de trï¿½nsito
 -- AUTHOR       : Aline Nunes e Davi Schmitz
 -- FINALITY     : 
 -- DEPENDENCIES : 
@@ -14,8 +14,6 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -------------------------------------------------------------------------------
 -- ENTITY
@@ -23,8 +21,17 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity semaforo is
 	port (
-    Lh: IN std_logic;
-    A: OUT std_logic
+    S1: IN std_logic;
+    S0: IN std_logic;
+
+    MR: OUT std_logic;
+    MY: OUT std_logic;
+    MG: OUT std_logic;
+    SR: OUT std_logic;
+    SY: OUT std_logic;
+    SG: OUT std_logic;
+    TL: OUT std_logic;
+    TS: OUT std_logic
   );
 end semaforo;
 
@@ -51,7 +58,10 @@ architecture semaforo of semaforo is
 	-----------------------------------
 	-- Statments of signals
 	-----------------------------------
-  signal aux1_s: std_logic := '0';
+  signal so1: std_logic := '0';
+  signal so2: std_logic := '0';
+  signal so3: std_logic := '0';
+  signal so4: std_logic := '0';
 
  	------------------
 	-- Port Mapping --
@@ -60,7 +70,19 @@ begin
 	-----------------------------
 	-- Asynchronous assignments --
 	-----------------------------
+  so1 <= not S1 and not S0;
+  so2 <= not S1 and S0;
+  so3 <= S1 and S0;
+  so4 <= S1 and not S0;
 
+  MR <= so3 or so4;
+  MY <= so2;
+  MG <= so1;
+  SR <= so1 or so2;
+  SY <= so4;
+  SG <= so3;
+  TL <= so1 or so3;
+  TS <= so2 or so4;
   ---------------
 	--  Process  --
 	---------------
